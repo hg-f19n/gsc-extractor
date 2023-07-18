2b253ade2af13989447382af003a41034688c6d2
hg-f19n <hg@f19n.com>
2023-07-18 12:02:30 +0200

    finsihed first navigation to crawl stats / need to optimize login process

--- main.js
+++ main.js
@@ -4,24 +4,78 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+let siteUrl = "";
 
-  // Go to Google login page
-  await page.goto("https://accounts.google.com/signin/v2/identifier");
-
-  // Wait for user to manually login
-  await new Promise((resolve) =>
-    readline.question("After logging in, press ENTER to continue.", resolve)
-  );
-
-  // Replace 'your-console-url' with the console URL you want to capture
-  await page.goto(
-    "https://search.google.com/search-console/settings/crawl-stats?resource_id=https%3A%2F%2Fwww.fullstackoptimization.com%2F&hl=en"
-  );
-  await page.screenshot({ path: "console.png" });
-
+readline.question("Enter your site URL: ", async (url) => {
+  siteUrl = encodeURIComponent(url);
   readline.close();
-  await browser.close();
-})();
+
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Go to Google login page
+    await page.goto("https://accounts.google.com/signin/v2/identifier");
+
+    console.log("You have 2 minutes to login.");
+
+    // Wait for user to login or timeout after 2 minutes
+    await Promise.race([
+      page.waitForNavigation(), // waits for the page to load
+      new Promise((_, reject) =>
+        setTimeout(() => reject(new Error("Timeout after 2 minutes")), 120000)
+      ), // 2 minute timeout
+    ]);
+
+    // Replace 'your-console-url' with the console URL you want to capture
+    const crawlStatsUrl = `https://search.google.com/search-console/settings/crawl-stats/drilldown?resource_id=${siteUrl}&file_type=1&hl=en`;
+    await page.goto(crawlStatsUrl);
+
+    const waitForElementByXPath = async (xpath, timeout = 120000) => {
+      const element = await page.waitForXPath(xpath, { timeout });
+      return element;
+    };
+
+    // Click on 'Average response time' button
+    const responseTimeButton = await waitForElementByXPath(
+      "//div[contains(., 'Average response time') and @role='button']"
+    );
+    if (responseTimeButton) {
+      await responseTimeButton.click();
+    } else {
+      throw new Error("Cannot find 'Average response time' button.");
+    }
+
+    // Add a delay to let the page load
+    await page.waitForTimeout(2000); // Wait for 2 seconds
+
+    // Click on 'Total crawl requests' button
+    const totalCrawlRequestsButton = await waitForElementByXPath(
+      "//div[contains(., 'Total crawl requests') and @role='button']"
+    );
+    if (totalCrawlRequestsButton) {
+      await totalCrawlRequestsButton.click();
+    } else {
+      throw new Error("Cannot find 'Total crawl requests' button.");
+    }
+
+    // Add a delay to let the page load
+    await page.waitForTimeout(2000); // Wait for 2 seconds
+
+    // Take screenshot of the specific element
+    const element = await page.$(
+      '[data-scorecard-title-0="Total crawl requests"]'
+    );
+    if (element) {
+      await element.screenshot({ path: "crawlStats.png" });
+    } else {
+      throw new Error(
+        "Cannot find element with 'data-scorecard-title-0=\"Total crawl requests\"'."
+      );
+    }
+
+    await browser.close();
+  } catch (error) {
+    console.error("An error occurred: ", error);
+  }
+});
